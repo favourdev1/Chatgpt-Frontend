bf8c853f6c1225d627a4d34b2b2c27178f11d3af
Favourdev1 <favourapps17@gmail.com>
2024-07-08 00:53:59 +0100

    minor change

--- src/components/Containers/mainContainer.jsx
+++ src/components/Containers/mainContainer.jsx
@@ -10,6 +10,7 @@ const MainContainer = () => {
   let [allMessages, setAllMessages] = useState([]); // State to store message
   let [isLoading, setIsLoading] = useState(false);
   const messagesEndRef = useRef(null);
+let [errorText, setErrorText] = useState("");
 
 
 
@@ -33,9 +34,14 @@ const MainContainer = () => {
     sendRequest(data);
   };
 
+
+  const clearErrorText = () => {
+    setErrorText("");
+  };
+
   const sendRequest = async (message) => {
-    const url = "https://chatgpt-backend-2qe8.onrender.com/";
- 
+    let url = "https://chatgpt-backend-2qe8.onrender.com/";
+    url = "http://localhost:3001/"
     let userMessage = {
       role: "user",
       text: message,
@@ -58,18 +64,27 @@ const MainContainer = () => {
         role: "assistant",
         text: response.data.data[0].text,
       };
-
+     
 
       setAllMessages((prevMessages) => [...prevMessages, aiMessage]);
 
-      console.log(allMessages);
+      // console.log(allMessages);
     } catch (error) {
-      console.log(error);
+      setIsLoading(false);
+      setErrorText(error.response.data.message);
+      // const timer = setTimeout(() => {
+      //  setErrorText("");
+      // }, 3000);
+      console.log(error.response.data.message);
+      // return () => clearTimeout(timer);
+      // console.log(errorText)
     }
   };
 
+
+
   return (
-    <div className="flex flex-col items-center   px-4 py-4  bg-transparent flex-1">
+    <div className="flex flex-col items-center   px-4 py-4  bg-transparent flex-1 relative">
       <div className="w-full xl:w-1/2   h-[85vh]  text-sm  overflow-y-scroll no-scrollbar  ">
         {allMessages.length < 1 ? (
           <Splashchat />
@@ -77,7 +92,7 @@ const MainContainer = () => {
           <div id="messagecontainer" className="pt-14">
             {allMessages.map((message, index) => (
               <div className="text-left  w-full mt-3 mb-5" key={index}>
-                <div className="flex items-start">
+                <div className="flex items-start prose">
                   <div className="flex-shrink-0">
              
                     {message.role == "user" ? (
@@ -101,8 +116,13 @@ const MainContainer = () => {
           </div>
         )}
         {/* <p>Message from Child: {sentMessage}</p> */}
+
+        {/* alert like react alert*/}
+   
+
       </div>
-      <MessageInput onSendData={handleDataFromChild} isLoading={isLoading} />
+      <MessageInput onSendData={handleDataFromChild} isLoading={isLoading} errorText={errorText} clearError={clearErrorText} />
+     
     </div>
   );
 };
--- src/components/messageInput.jsx
+++ src/components/messageInput.jsx
@@ -1,107 +1,131 @@
-import React, { useState } from "react";
+/** @format */
 
-const MessageInput = ({ onSendData, isLoading }) => {
-  const [text, setText] = useState('');
-let [istyping,setIsTyping]=useState(false)
+import React, { useState,useEffect } from "react";
+
+const MessageInput = ({ onSendData, isLoading, errorText,clearError }) => {
+	const [text, setText] = useState("");
+	let [istyping, setIsTyping] = useState(false);
+  const [translateClass, setTranslateClass] = useState('translate-y-48');
+	
   const handleClick = (message) => {
-    onSendData(message);
-    setTimeout(() => {
-      setText('');
-    }, 100);
-  };
+		onSendData(message);
+		setTimeout(() => {
+			setText("");
+		}, 100);
+	};
 
-  const handleKeyPress = (event) => {
-    if (isLoading) return;
-
-    if (event.key === 'Enter' && !event.shiftKey) {
-      event.preventDefault();
-      handleClick(text);
+console.log(errorText)
+  useEffect(() => {
+    if (errorText ) {
+      setTranslateClass('translate-y-0');
+      const timer = setTimeout(() => {
+        setTranslateClass('translate-y-48');
+        clearError(); 
+      }, 3000);
+      return () => clearTimeout(timer);
     }
-  };
-  
-  function checkIfisTyping(text) {
-  
-    if (text.length > 0) {
-      setIsTyping(true)
-    }
-    else {
-      setIsTyping(false)
-    }
-    setText(text)
+  }, [errorText]);
 
-  }
+	const handleKeyPress = (event) => {
+		if (isLoading) return;
 
-  return (
-    <div className="flex items-center flex-col justify-between px-1 md:px-4 pt-4 w-full">
+		if (event.key === "Enter" && !event.shiftKey) {
+			event.preventDefault();
+			handleClick(text);
+		}
+	};
 
-      <div className="w-full xl:w-1/2">
-        <div className={`transition-all px-4  rounded-xl flex items-center border border-chatgpt-border-gray mx-auto py-1 `}>
-          <textarea
-            onChange={(e) => checkIfisTyping(e.target.value)}
-            id="question"
-            tabindex="0"
-            dir="auto"
-            rows="1"
-            value={text}
-            onKeyPress={handleKeyPress}
-            // onChange={(event) => setText(event.target.value)}
-            placeholder="Message ChatGPT…"
-            className="m-0 w-full text-base border-0 rounded-2xl resize-none bg-transparent focus:ring-0 focus-visible:ring-0 outline-0 dark:bg-transparent py-[10px] pr-10 md:py-3 md:pr-12 max-h-[25dvh] max-h-52 placeholder-white/50 dark:placeholder-white/50 pl-3 md:pl-4"
-            style={{ overflowY: "hidden" }}
-          ></textarea>
-          <button
-            
-            id="sendButton"
-            onClick={() => handleClick(text)}
-            className={`w-max p-1.5 ${!istyping?'text-chatgpt-dark-gray bg-chatgpt-border-gray':'bg-white text-chatgpt-dark-gray'} rounded-xl  ${!isLoading?'scale-1':'scale-0'}  p-2  text-white  duration-200 `}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              className=""
-            >
-              <path
-                d="M7 11L12 6L17 11M12 18V7"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </svg>
-          </button>
-          {isLoading && (
-            <div className="ml-2">
-              <svg
-                className="animate-spin h-5 w-5 text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            </div>
-          )}
-        </div>
-      </div>
-      <p className="text-center w-full text-xs text-gray-400 mt-4 mb-0">
-        ChatGPT can make mistakes. Consider checking important information.
-      </p>
-    </div>
-  );
+	function checkIfisTyping(text) {
+		if (text.length > 0) {
+			setIsTyping(true);
+		} else {
+			setIsTyping(false);
+		}
+		setText(text);
+	}
+  console.log(errorText); 
+	return (
+		<div className="flex items-center flex-col justify-between px-1 md:px-4 pt-4 w-full">
+			<div className="w-full xl:w-1/2">
+				<div
+					className={`transition-all px-4  rounded-xl flex items-center border border-chatgpt-border-gray mx-auto py-1 `}
+				>
+					<textarea
+						onChange={(e) => checkIfisTyping(e.target.value)}
+						id="question"
+						tabindex="0"
+						dir="auto"
+						rows="1"
+						value={text}
+						onKeyPress={handleKeyPress}
+						// onChange={(event) => setText(event.target.value)}
+						placeholder="Message ChatGPT…"
+						className="m-0 w-full text-base border-0 rounded-2xl resize-none bg-transparent focus:ring-0 focus-visible:ring-0 outline-0 dark:bg-transparent py-[10px] pr-10 md:py-3 md:pr-12 max-h-[25dvh] max-h-52 placeholder-white/50 dark:placeholder-white/50 pl-3 md:pl-4"
+						style={{ overflowY: "hidden" }}
+					></textarea>
+					<button
+						id="sendButton"
+						onClick={() => handleClick(text)}
+						className={`w-max p-1.5 ${
+							!istyping
+								? "text-chatgpt-dark-gray bg-chatgpt-border-gray"
+								: "bg-white text-chatgpt-dark-gray"
+						} rounded-xl  ${
+							!isLoading ? "scale-1" : "scale-0"
+						}  p-2    duration-200 `}
+					>
+						<svg
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							className=""
+						>
+							<path
+								d="M7 11L12 6L17 11M12 18V7"
+								stroke="currentColor"
+								strokeWidth="2"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							></path>
+						</svg>
+					</button>
+					{isLoading && (
+						<div className="ml-2">
+							<svg
+								className="animate-spin h-5 w-5 text-gray-500"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<circle
+									className="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									strokeWidth="4"
+								></circle>
+								<path
+									className="opacity-75"
+									fill="currentColor"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								></path>
+							</svg>
+						</div>
+					)}
+				</div>
+			</div>
+			<p className="text-center w-full text-xs text-gray-400 mt-4 mb-0">
+				ChatGPT can make mistakes. Consider checking important information.
+			</p>
+
+			<div className={`flex items-center justify-center bg-red-500/40 fixed top-100 rounded-xl py-4 px-10 text-sm font-light duration-500  ${translateClass}`} id="errorDummy">
+        {errorText}
+       
+			</div>
+		</div>
+	);
 };
 
 export default MessageInput;
